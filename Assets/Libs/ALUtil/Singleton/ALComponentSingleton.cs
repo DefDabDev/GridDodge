acf199bb211a35a7c32fd3fcd45e91d7d8c34ead
Kim Deokwon <64bit_decryption@naver.com>
2017-05-10 13:54:54 +0900

    Add ALUtil

---
+++ Assets/Libs/ALUtil/Singleton/ALComponentSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL.ALUtil
+{
+    public class ALComponentSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        static T _instance = null;
+        /// <summary>
+        /// Singleton객체를 리턴 함
+        /// </summary>
+        public static T instance { get { return _instance ?? GetInstanceObject(); } }
+
+        void Awake()
+        {
+            _instance = this as T;
+        }
+
+        static T GetInstanceObject()
+        {
+            _instance = GameObject.FindObjectOfType<T>().GetComponent<T>();
+            return _instance ?? null;
+        }
+    }
+}
---
+++ Assets/Libs/ALUtil/Singleton/ALSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL.ALUtil
+{
+    /// <summary>
+    /// Normal Singleton
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class ALSingleton<T> where T : ALSingleton<T>, new()
+    {
+        static T _instance = null;
+        public static T instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new T());
+            }
+        }
+    }
+}

6213b3f222f6cf8e3649ddc9d2867d27d3301d1c
Kim Deokwon <64bit_decryption@naver.com>
2017-05-10 14:14:41 +0900

    Add State, StateCtrl

---
+++ Assets/Anywhere/State.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State {
+
+    private StateController _stateController;
+    public StateController stateController { get { return _stateController; } set { _stateController = value; } }
+
+    public abstract void OnEnter(params object[] datas);    
+    public abstract IEnumerator OnUpdate(params object[] datas);  
+    public abstract void OnExit(params object[] datas);    
+}
---
+++ Assets/Anywhere/StateController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AL.ALUtil;
+using System;
+
+public class StateController : ALComponentSingleton<StateController> {
+
+    [SerializeField]
+    private List<State> _stateList;
+    public List<State> stateList { get { return _stateList; } }
+    private Dictionary<string, State> _stateDictionary;
+
+    [SerializeField]
+    private string _nextStateName;
+
+    [SerializeField]
+    private string _currentStateName;
+
+    [SerializeField]
+    private string _beforeStateName;
+
+    [SerializeField]
+    private bool _isExcuteAble;
+    public bool isExcuteAble { get { return _isExcuteAble; } set { _isExcuteAble = value; } }
+
+    private State _currentState;
+    public State currentState { get { return _currentState; } }
+
+	private void Awake ()
+    {
+        _stateDictionary = new Dictionary<string, State>();
+
+        for (int i = 0; i < stateList.Count; i++)
+        {
+            stateList[i].stateController = this;
+            _stateDictionary.Add(stateList[i].GetType().Name, stateList[i]);
+        }
+
+        if (stateList.Count > 0)
+        {
+            _currentState = stateList[0];
+            _currentStateName = currentState.GetType().Name;
+            currentState.OnEnter();
+        }
+    }
+
+    public void Excute(params object[] datas)
+    {
+        if (_currentState != null && _isExcuteAble)
+        {
+            StartCoroutine(_currentState.OnUpdate());
+            _isExcuteAble = false;
+        }
+    }
+
+    public void ChangeState(string name, params object[] datas)
+    {
+        _beforeStateName = _currentStateName;
+
+        if (currentState != null)
+            currentState.OnExit(datas);
+
+        _currentState = _stateDictionary[name];
+        currentState.OnEnter(datas);
+
+        _currentStateName = currentState.GetType().Name;
+        _isExcuteAble = true;
+    }
+
+    public void ChangeBeforeState(params object[] datas)
+    {
+        if (currentState != null)
+            currentState.OnExit(datas);
+        _currentState = _stateDictionary[_beforeStateName];
+        _currentState.OnEnter(datas);
+
+        _currentStateName = _currentState.GetType().Name;
+        _isExcuteAble = true;
+    }
+}
